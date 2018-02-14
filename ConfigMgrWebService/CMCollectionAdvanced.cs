0e20a7691cb219a06e4e1ba314d29d1d286c879c
GlenR2D2 <36387833+GlenR2D2@users.noreply.github.com>
2018-02-14 14:26:03 +0100

    Add files via upload

---
+++ ConfigMgrWebService/CMCollectionAdvanced.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace ConfigMgrWebService
+{
+
+    public class CMCollectionVariables
+    {
+        public string Name { get; set; }
+        public string CollectionID { get; set; }
+        public List<CMVariables> Variable { get; set; }
+    }
+
+    public class CMCollectionAdvanced
+    {
+        public string Name { get; set; }
+        public string CollectionID { get; set; }
+        public List<CMVariables> Variable { get; set; }
+        public System.Collections.ArrayList AdhesionRules { get; set; }
+    }
+
+    public class CMVariablesSettings
+    {
+        public string CollectionID { get; set; }
+        public List<CMVariables> Variable { get; set; }
+    }
+
+    public class CMVariables
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+}
