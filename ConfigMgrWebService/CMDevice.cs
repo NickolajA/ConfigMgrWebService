859457bda26ece16d099a9704ebc8df21ee80b03
GlenR2D2 <36387833+GlenR2D2@users.noreply.github.com>
2018-02-25 21:35:31 +0100

    Add files via upload

---
+++ ConfigMgrWebService/CMDevice.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ConfigMgrWebService
+{
+    public class CMDevice
+    {
+        public string Name { get; set; }
+        public List<CMDeviceVariable> Variable { get; set; }
+    }
+
+    public class CMDeviceByID
+    {
+        public string ResourceID { get; set; }
+        public List<CMDeviceVariable> Variable { get; set; }
+    }
+
+    public class CMDeviceVariable
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
