ad934a82ffdf8f196aa7918d871c4307efd9ae16
Flavio Piagno <20758831+Piagno@users.noreply.github.com>
2019-05-27 11:31:49 +0200

    Add getDeviceByName

---
+++ ConfigMgrWebService/CMDevice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConfigMgrWebService
+{
+    public class CMDevice
+    {
+        public string Name { get; set; }
+    }
+}
